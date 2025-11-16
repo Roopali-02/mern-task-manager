2a48a7f3dc6e427985fbc8e63cf9f6ac0746b706
Roopali <roopalipatil90@gmail.com>
2025-11-16 17:27:24 +0530

    Change base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+	baseURL: "https://task-manager-api-ctih.onrender.com",
 });
 
 // Add token automatically in every request (if exists)
